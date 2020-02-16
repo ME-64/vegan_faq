8b34242d749553d16482e6224ec0d6498966be1f
ME-64 <ME-64>
2020-02-16 16:24:00 +0000

    changed formatting to be better on mobile

--- vegan_faq/app.py
+++ vegan_faq/app.py
@@ -11,15 +11,11 @@ from about import About
 
 
 
-#navigation = st.empty()
-st.title(
-''':recycle: :recycle: :recycle: :recycle: :recycle: :recycle: :recycle: :recycle: :recycle: :recycle: :recycle: :recycle: :recycle: :recycle: '''
-)
-
 st.title('The **S**uprisingly **E**ducated **V**eganBot')
 
-navigation = st.sidebar.selectbox('Navigation:', ('Home', 'About'))
-
+#navigation = st.sidebar.selectbox('Navigation:', ('Home', 'About'))
+# removing navigation feature until streamlit handles it better on mobile
+navigation = 'Home'
 
 if navigation == 'Home':
     Home()
--- vegan_faq/home.py
+++ vegan_faq/home.py
@@ -34,6 +34,41 @@ def Home():
     df = utils.load_data()
     model = utils.load_model()
     
+    st.markdown(
+    """
+    ***
+    # About
+    ## What is SEV?
+    **SEV** ( The 'Suprisingly Educated VeganBot') is a question-answering bot that tries to answer your *veganism related* questions!
+    
+    
+    ## How does SEV work?    
+    SEV tries to match your question to its stored knowledge bank - and then return the most relevant response.
+    SEV is a 'closed domain' QA bot, and thus if the answer is not found in one of SEV's corpora, he will have a hard time answering
+    the question!
+
+    
+    ## Tell me more about SEV's knowledge bank    
+    SEV's knowledge bank was curated from various sources such as: reddit, wikipedia, bbc news, & vegan related websites.
+    The set of questions & answers is quality controlled to ensure the responses accurately represent veganism!
+    
+    ***
+    
+    [![github][1]][2]
+    [![email][3]][4]
+     ![version][5]
+    
+    [1]:  https://img.shields.io/static/v1?label=github&message=vegan_faq&color=informational&logo=github
+    [2]:  https://www.github.com/ME-64/vegan_faq
+    [3]:  https://img.shields.io/static/v1?label=contact&message=miloelliott64@gmail.com&color=green&logo=gmail
+    [4]:  mailto:miloelliott64@gmail.com
+    [5]:  https://img.shields.io/static/v1?label=version&message=Proof%20of%20Concept&color=red
+    
+    """
+    
+    
+    )
+    
     if user_q == '':
         pass
     
